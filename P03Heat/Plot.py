d4ca2bc041b07797e263b3fcee8152afd7b71b0b
pppppass <lzh2016p@pku.edu.cn>
2018-07-15 15:36:13 +0800

    Update report for Project 3

--- P03Heat/Plot.py
+++ P03Heat/Plot.py
@@ -5,6 +5,10 @@
 
 
 import shelve
+import matplotlib
+matplotlib.use("pgf")
+matplotlib.rcParams["text.usetex"] = True
+matplotlib.rcParams["font.family"] = "serif"
 from matplotlib import pyplot
 
 
@@ -21,7 +25,7 @@ with shelve.open('Result') as db:
 
 
 print("HL")
-print("$ 1 / \\tau $ CR $\\mu_{x}$, $\\mu_{y}$ CR Explicit CR Implicit CR Crank--Nicolson BL")
+print("$ 1 / \\tau $ CR $\\mu$ CR Explicit CR Implicit CR Crank--Nicolson BL")
 print("HL")
 for i, m in enumerate(m_list):
     print("{} CR {:.2e} CR {:.5e} CR {:.5e} CR {:.5e} BL".format(
@@ -57,10 +61,10 @@ mtds = [
 
 
 print("HL")
-print("Method CR Time (\\Si{s}) CR Iter. CR Error to $u$ CR Rel. Error to $u$ CR Error to $u_{\\text{s}}$ CR Rel. Error to $u_{\\text{s}}$ BL")
+print("Method CR Time (\\Si{s}) CR Iter. CR Err. $u$ CR Rel. $u$ CR Err. $u_{\\text{s}}$ CR Rel. $u_{\\text{s}}$ BL")
 print("HL")
 for i in range(5):
-    print("{} CR {:.5f} CR {} CR {:.5e} CR {:.3f} CR {:.5e} CR {:.3f} BL".format(mtds[i], *rt[0][i]))
+    print("{} CR {:.5f} CR {} CR {:.5e} CR {:.3e} CR {:.5e} CR {:.3e} BL".format(mtds[i], *rt[0][i]))
     print("HL")
 
 
@@ -68,7 +72,7 @@ for i in range(5):
 
 
 print("HL")
-print("Method CR Time (\\Si{s}) CR Iter. CR Error to $u$ CR Rel. Error to $u$ CR Error to $u_{\\text{s}}$ CR Rel. Error to $u_{\\text{s}}$ BL")
+print("Method CR Time (\\Si{s}) CR Iter. CR Err. $u$ CR Rel. $u$ CR Err. $u_{\\text{s}}$ CR Rel. $u_{\\text{s}}$ BL")
 print("HL")
 for i in range(5):
     print("{} CR {:.5f} CR {} CR {:.5e} CR {:.3f} CR {:.5e} CR {:.3f} BL".format(mtds[i], *rt[1][i]))
@@ -86,7 +90,7 @@ with shelve.open('Result') as db:
 # In[9]:
 
 
-pyplot.figure(figsize=(8.0, 6.0))
+pyplot.figure(figsize=(6.0, 4.5))
 pyplot.scatter(m_list[14:], rt[0][14:], 5.0)
 pyplot.plot(m_list[14:], rt[0][14:], label="Exp.")
 pyplot.scatter(m_list, rt[1], 5.0)
@@ -97,7 +101,7 @@ pyplot.semilogx()
 pyplot.semilogy()
 pyplot.ylim(1.0e-12, 3.0e-7)
 pyplot.xlabel("$ 1 / \\tau $")
-pyplot.ylabel("$\\epsilon$")
+pyplot.ylabel("Error")
 pyplot.legend()
 pyplot.tight_layout()
 pyplot.show()
@@ -128,17 +132,46 @@ labels = [
     "CN, $ \\tau = h / 6 $",
     "CN, $ \\tau = h / 8 $",
 ]
+styles = [
+    "-",
+    "-",
+    "-",
+    "-",
+    "--",
+    "--",
+    "--",
+    ":",
+    ":",
+    ":",
+]
+colors = [
+    0.0,
+    0.33,
+    0.67,
+    1.0,
+    0.0,
+    0.5,
+    1.0,
+    0.0,
+    0.5,
+    1.0,
+]
+cmap = pyplot.get_cmap("plasma")
+cnorm = matplotlib.colors.Normalize(vmin=0.0, vmax=1.2)
+cmpbl = matplotlib.cm.ScalarMappable(norm=cnorm, cmap=cmap)
 
 
 # In[12]:
 
 
-pyplot.figure(figsize=(8.0, 6.0))
-for i in range(9):
-    pyplot.scatter(n_list, rt[i], 5.0)
-    pyplot.plot(n_list, rt[i], label=labels[i])
+pyplot.figure(figsize=(6.0, 4.5))
+for i in range(10):
+    pyplot.scatter(n_list, rt[i], 5.0, c=cmpbl.to_rgba(colors[i]))
+    pyplot.plot(n_list, rt[i], c=cmpbl.to_rgba(colors[i]), label=labels[i], linestyle=styles[i])
 pyplot.semilogx()
 pyplot.semilogy()
+pyplot.xlabel("$ 1 / h $")
+pyplot.ylabel("Error")
 pyplot.legend()
 pyplot.tight_layout()
 pyplot.show()
